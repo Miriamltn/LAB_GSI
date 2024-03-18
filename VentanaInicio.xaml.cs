75f2b2448cca9f1207f541015cfb27d9b8ae732a
Miriamltn <114506193+Miriamltn@users.noreply.github.com>
2024-03-18 12:38:42 +0100

    Ventan de inicio y paso a las siguientes ventanas

---
+++ VentanaInicio.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace LAB_GSI
+{
+    /// <summary>
+    /// Lógica de interacción para VentanaInicio.xaml
+    /// </summary>
+    public partial class VentanaInicio : Window
+    {
+        public VentanaInicio()
+        {
+            InitializeComponent();
+            
+        }
+
+
+        private void btn_empezar_Click(object sender, RoutedEventArgs e)
+        {
+            MainWindow ventanaNueva = new MainWindow();
+            ventanaNueva.Show();
+            this.Close();
+        }
+    }
+}
