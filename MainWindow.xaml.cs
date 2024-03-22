c7ffbeb0deb93472f55d3566d71d50075d2ada50
Miriamltn <114506193+Miriamltn@users.noreply.github.com>
2024-03-22 14:57:35 +0100

    Creadas pestañas, y probar meter infromacion

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -23,6 +23,36 @@ namespace LAB_GSI
         public MainWindow()
         {
             InitializeComponent();
+            toggleButton1.Checked += ToggleButton_Checked;
+            toggleButton1.Unchecked += ToggleButton_Unchecked;
+            stackPanel1.Visibility = Visibility.Collapsed;
         }
+
+        private void ToggleButton_Checked(object sender, RoutedEventArgs e)
+        {
+            stackPanel1.Visibility = Visibility.Visible;
+            // Aquí puedes realizar acciones adicionales cuando se muestra el contenido
+        }
+
+        private void ToggleButton_Unchecked(object sender, RoutedEventArgs e)
+        {
+            stackPanel1.Visibility = Visibility.Collapsed;
+            // Aquí puedes realizar acciones adicionales cuando se oculta el contenido
+        }
+      
+        private void TabControl_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            // Verificar si la pestaña seleccionada es "Dudas"
+            if (P_Dudas.IsSelected)
+            {
+                stackPanel1.Visibility = Visibility.Visible;
+            }
+            else
+            {
+                stackPanel1.Visibility = Visibility.Collapsed;
+            }
+        }
+
+
     }
 }
