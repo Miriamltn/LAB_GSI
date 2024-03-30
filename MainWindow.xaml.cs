67a2f198825a646c88ad27fbcbc7779631a50a00
Miriamltn <114506193+Miriamltn@users.noreply.github.com>
2024-03-30 14:23:51 +0100

    Ventana Información completada, avanzado expander Nutricion en Dudas y diseño ventana ejercicios en proceso

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -12,7 +12,6 @@ namespace LAB_GSI
         public MainWindow()
         {
             InitializeComponent();
-            //stackPanel1.Visibility = Visibility.Collapsed;
             CargarDefinicionDesdeXML();
         }
 
@@ -32,8 +31,6 @@ namespace LAB_GSI
                 if (seccionDefinicion != null)
                 {
                     string textoDefinicion = seccionDefinicion.SelectSingleNode("texto").InnerText;
-
-                    // Mostrar la definición en el TextBox tb_definicion
                     tb_definicion.Text = textoDefinicion;
                 }
                 else
@@ -46,20 +43,16 @@ namespace LAB_GSI
 
                 if (seccionSintomas != null)
                 {
-                    // Obtener los elementos 'item' dentro de 'texto'
                     XmlNodeList sintomasNodes = seccionSintomas.SelectNodes("texto/item");
 
                     if (sintomasNodes != null && sintomasNodes.Count > 0)
                     {
-                        // Construir el texto con los síntomas en líneas separadas
-                        string textoSintomas = "";
+                        StringBuilder textoSintomas = new StringBuilder();
                         foreach (XmlNode sintomaNode in sintomasNodes)
                         {
-                            textoSintomas += sintomaNode.InnerText + Environment.NewLine;
+                            textoSintomas.AppendLine($"- {sintomaNode.InnerText}");
                         }
-
-                        // Mostrar los síntomas en el TextBox tb_sintomas
-                        tb_sintomas.Text = textoSintomas.Trim();  // Trim para quitar el espacio adicional al final
+                        tb_sintomas.Text = textoSintomas.ToString().Trim();
                     }
                     else
                     {
@@ -72,50 +65,44 @@ namespace LAB_GSI
 
                 if (seccionFases != null)
                 {
-                    // Obtener todos los nodos "etapa" dentro de "Fases"
                     XmlNodeList etapasNodes = seccionFases.SelectNodes("etapa");
 
                     if (etapasNodes != null && etapasNodes.Count > 0)
                     {
-                        // Construir el texto con las fases en líneas separadas
-                        string textoFases = "";
+                        StringBuilder textoFases = new StringBuilder();
                         foreach (XmlNode etapaNode in etapasNodes)
                         {
                             string nombreEtapa = etapaNode.SelectSingleNode("nombre").InnerText;
                             string descripcionEtapa = etapaNode.SelectSingleNode("descripcion").InnerText;
 
-                            textoFases += $"{nombreEtapa}: {descripcionEtapa}" + Environment.NewLine + Environment.NewLine;
+                            textoFases.AppendLine($"{nombreEtapa}: {descripcionEtapa}");
                         }
-
-                        // Mostrar las fases en el TextBox tb_fases
-                        tb_fases.Text = textoFases.Trim();  // Trim para quitar el espacio adicional al final
+                        tb_fases.Text = textoFases.ToString().Trim();
                     }
                     else
                     {
                         tb_fases.Text = "No se encontraron fases en el archivo XML.";
                     }
                 }
+
+                // Obtener la sección "Tratamientos"
                 XmlNode seccionTratamientos = xmlDoc.SelectSingleNode("/informacion/seccion[@nombre='Tratamientos']");
 
                 if (seccionTratamientos != null)
                 {
-                    // Obtener los elementos 'tratamiento' dentro de 'Tratamientos'
                     XmlNodeList tratamientosNodes = seccionTratamientos.SelectNodes("etapa");
 
                     if (tratamientosNodes != null && tratamientosNodes.Count > 0)
                     {
-                        // Construir el texto con los tratamientos en líneas separadas
-                        string textoTratamientos = "";
+                        StringBuilder textoTratamientos = new StringBuilder();
                         foreach (XmlNode tratamientoNode in tratamientosNodes)
                         {
                             string nombreTratamiento = tratamientoNode.SelectSingleNode("nombre").InnerText;
                             string descripcionTratamiento = tratamientoNode.SelectSingleNode("descripcion").InnerText;
 
-                            textoTratamientos += $"{nombreTratamiento}: {descripcionTratamiento}" + Environment.NewLine + Environment.NewLine;
+                            textoTratamientos.AppendLine($"{nombreTratamiento}: {descripcionTratamiento}");
                         }
-
-                        // Mostrar los tratamientos en el TextBox tb_avances
-                        tb_avances.Text = textoTratamientos.ToString().Trim();  // Trim para quitar el espacio adicional al final
+                        tb_avances.Text = textoTratamientos.ToString().Trim();
                     }
                     else
                     {
@@ -126,17 +113,17 @@ namespace LAB_GSI
                 {
                     tb_avances.Text = "Sección 'Tratamientos' no encontrada en el archivo XML.";
                 }
+
+                // Obtener la sección "Nutricion"
                 XmlNode seccionNutricion = xmlDoc.SelectSingleNode("/informacion/seccion[@nombre='Nutricion']");
 
                 if (seccionNutricion != null)
                 {
-                    // Obtener los elementos 'texto' y 'item' dentro de 'informacion'
-                    XmlNodeList textoNodes = seccionNutricion.SelectNodes("informacion/texto");
-                    XmlNodeList itemNodes = seccionNutricion.SelectNodes("informacion/item");
+                    XmlNodeList textoNodes = seccionNutricion.SelectNodes("texto");
+                    XmlNodeList itemNodes = seccionNutricion.SelectNodes("item");
 
                     if (textoNodes != null && textoNodes.Count > 0 && itemNodes != null && itemNodes.Count > 0)
                     {
-                        // Construir el texto con la información de nutrición
                         StringBuilder textoNutricion = new StringBuilder();
                         foreach (XmlNode textoNode in textoNodes)
                         {
@@ -151,12 +138,11 @@ namespace LAB_GSI
                             textoNutricion.AppendLine($"- {itemNode.InnerText.Trim()}");
                         }
 
-                        // Mostrar la información de nutrición en el TextBox tb_nutricion
                         tb_nutricion.Text = textoNutricion.ToString().Trim();
                     }
                     else
                     {
-                        tb_nutricion.Text = "No se encontró la información de nutrición en el archivo XML.";
+                        tb_nutricion.Text = "No se encontró la información de nutrición en la sección 'Nutricion' del archivo XML.";
                     }
                 }
                 else
@@ -164,13 +150,12 @@ namespace LAB_GSI
                     tb_nutricion.Text = "Sección 'Nutricion' no encontrada en el archivo XML.";
                 }
 
-                XmlNode seccionActuar = xmlDoc.SelectSingleNode("/informacion/seccion[@nombre='Aceptación de la enfermedad']");
+                // Obtener la sección "Aceptación de la enfermedad"
+                XmlNode seccionActuar = xmlDoc.SelectSingleNode("/informacion/seccion[@nombre='Aceptacion de la enfermedad']");
 
                 if (seccionActuar != null)
                 {
                     string textoActuar = seccionActuar.SelectSingleNode("texto").InnerText;
-
-                    // Mostrar la definición en el TextBox tb_definicion
                     tb_comoActuar.Text = textoActuar;
                 }
                 else
@@ -178,13 +163,12 @@ namespace LAB_GSI
                     tb_comoActuar.Text = "Sección 'Aceptacion de la enfermedad' no encontrada en el archivo XML.";
                 }
 
+                // Obtener la sección "Alteraciones fisicas"
                 XmlNode seccionFisica = xmlDoc.SelectSingleNode("/informacion/seccion[@nombre='Alteraciones fisicas']");
 
                 if (seccionFisica != null)
                 {
                     string textoFisico = seccionFisica.SelectSingleNode("texto").InnerText;
-
-                    // Mostrar la definición en el TextBox tb_definicion
                     tb_fisicas.Text = textoFisico;
                 }
                 else
@@ -192,13 +176,12 @@ namespace LAB_GSI
                     tb_fisicas.Text = "Sección 'Alteraciones fisicas' no encontrada en el archivo XML.";
                 }
 
+                // Obtener la sección "Causas"
                 XmlNode seccionCausas = xmlDoc.SelectSingleNode("/informacion/seccion[@nombre='Causas']");
 
                 if (seccionCausas != null)
                 {
                     string textoCausas = seccionCausas.SelectSingleNode("texto").InnerText;
-
-                    // Mostrar la definición en el TextBox tb_definicion
                     tb_causas.Text = textoCausas;
                 }
                 else
@@ -206,13 +189,12 @@ namespace LAB_GSI
                     tb_causas.Text = "Sección 'Causas' no encontrada en el archivo XML.";
                 }
 
+                // Obtener la sección "Esperanza de vida"
                 XmlNode seccionEsperanza = xmlDoc.SelectSingleNode("/informacion/seccion[@nombre='Esperanza de vida']");
 
                 if (seccionEsperanza != null)
                 {
                     string textoEsperanza = seccionEsperanza.SelectSingleNode("texto").InnerText;
-
-                    // Mostrar la definición en el TextBox tb_definicion
                     tb_Esperanza.Text = textoEsperanza;
                 }
                 else
@@ -225,9 +207,14 @@ namespace LAB_GSI
                 tb_definicion.Text = "Error al cargar los datos desde el archivo XML: " + ex.Message;
                 tb_sintomas.Text = "Error al cargar los datos desde el archivo XML: " + ex.Message;
                 tb_fases.Text = "Error al cargar los datos desde el archivo XML: " + ex.Message;
+                tb_avances.Text = "Error al cargar los datos desde el archivo XML: " + ex.Message;
+                tb_nutricion.Text = "Error al cargar los datos desde el archivo XML: " + ex.Message;
+                tb_comoActuar.Text = "Error al cargar los datos desde el archivo XML: " + ex.Message;
+                tb_fisicas.Text = "Error al cargar los datos desde el archivo XML: " + ex.Message;
+                tb_causas.Text = "Error al cargar los datos desde el archivo XML: " + ex.Message;
+                tb_Esperanza.Text = "Error al cargar los datos desde el archivo XML: " + ex.Message;
             }
         }
-
 
         private void TabControl_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
@@ -248,7 +235,7 @@ namespace LAB_GSI
             Expander expanded = sender as Expander;
 
             // Crea una lista con todos tus Expanders
-            List<Expander> allExpanders = new List<Expander> { Expander1, Expander2, Expander3, Expander4, Expander5, Expander6, Expander6, Expander8 };
+            List<Expander> allExpanders = new List<Expander> { Expander1, Expander2, Expander3, Expander4, Expander5, Expander6, Expander6, Expander8, Expander9, Expander17, Expander18, Expander19, Expander20 };
 
             // Cierra todos los Expanders excepto el que se expandió
             foreach (Expander expander in allExpanders)
@@ -261,5 +248,19 @@ namespace LAB_GSI
         }
 
 
+        // Metodo para iniciar el video de youtube
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            string enlaceYouTube = "https://www.youtube.com/watch?v=hIOWIJYVtXg";
+
+            // Usa Process.Start para abrir el enlace en el navegador predeterminado del sistema.
+            System.Diagnostics.Process.Start(new System.Diagnostics.ProcessStartInfo(enlaceYouTube) { UseShellExecute = true });
+        }
+
+        private void MostrarImagen_Click(object sender, RoutedEventArgs e)
+        {
+            FotoNutricion ventanaNutricion = new FotoNutricion();
+            ventanaNutricion.Show();
+        }
     }
 }
