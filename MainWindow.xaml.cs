dea2f129fc1cd38bbe72a5f18420bb8077bd42ea
aadriii03 <163842993+aadriii03@users.noreply.github.com>
2024-04-09 20:56:54 +0200

    Acabado diseño y funcionalidad de las peliculas de la seccion recursos

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -353,9 +353,9 @@ namespace LAB_GSI
             // Crea una lista con todos tus Expanders
             List<Expander> allExpanders = new List<Expander> { Expander1, Expander2, Expander3, Expander4, Expander5, 
                 Expander6, Expander6, Expander8, Expander9,Expander10, Expander11, Expander12, Expander13, 
-                Expander14, Expander15, Expander16, Expander17, Expander18, Expander19, Expander20, Expander21, Expander22, 
-                Expander23, Expander24, Expander25, Expander26, Expander27, Expander28, Expander29, Expander30, Expander31, Expander32, Expander33,
-                Expander34, Expander35, Expander36, Expander37, Expander38, Expander39, Expander40, Expander41, Expander42, Expander43, Expander44};
+                Expander14, Expander15, Expander16, Expander17, Expander18, Expander19, Expander20, 
+                Expander29, Expander30, Expander31, Expander32, Expander33, Expander34, Expander35, 
+                Expander36, Expander37, Expander38, Expander39, Expander40, Expander41, Expander42, Expander43, Expander44};
 
             // Cierra todos los Expanders excepto el que se expandió
             foreach (Expander expander in allExpanders)
@@ -463,9 +463,39 @@ namespace LAB_GSI
             System.Diagnostics.Process.Start(new System.Diagnostics.ProcessStartInfo(enlace) { UseShellExecute = true });
         }
 
-        private void AbrirLibro1(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        private void AbrirVideo1(object sender, RoutedEventArgs e)
         {
+            string enlace = "https://www.bing.com/videos/riverview/relatedvideo?&q=peliculas+gratis+en+youtube+para+el+alzheimer&&mid=786579F14ED764F3633F786579F14ED764F3633F&&FORM=VRDGAR";
 
+            System.Diagnostics.Process.Start(new System.Diagnostics.ProcessStartInfo(enlace) { UseShellExecute = true });
+        }
+
+        private void AbrirVideo2(object sender, RoutedEventArgs e)
+        {
+            string enlace = "https://www.youtube.com/watch?v=6lCjuNY5flY";
+
+            System.Diagnostics.Process.Start(new System.Diagnostics.ProcessStartInfo(enlace) { UseShellExecute = true });
+        }
+
+        private void AbrirVideo3(object sender, RoutedEventArgs e)
+        {
+            string enlace = "https://www.youtube.com/watch?v=BHCJ_BAvGQ4";
+
+            System.Diagnostics.Process.Start(new System.Diagnostics.ProcessStartInfo(enlace) { UseShellExecute = true });
+        }
+
+        private void AbrirVideo4(object sender, RoutedEventArgs e)
+        {
+            string enlace = "https://www.bing.com/videos/riverview/relatedvideo?&q=peliculas+para+personas+con+alzheimer&&mid=7037674D78C1BDE24D6A7037674D78C1BDE24D6A&&FORM=VRDGAR";
+
+            System.Diagnostics.Process.Start(new System.Diagnostics.ProcessStartInfo(enlace) { UseShellExecute = true });
+        }
+
+        private void AbrirVideo5(object sender, RoutedEventArgs e)
+        {
+            string enlace = "https://www.youtube.com/watch?v=ABUkyPSfjz8";
+
+            System.Diagnostics.Process.Start(new System.Diagnostics.ProcessStartInfo(enlace) { UseShellExecute = true });
         }
     }
 }
